e09755c1e7e0a7bb363177c531b42552f5b83f45
Topscorer101 <46632281+Topscorer101@users.noreply.github.com>
2019-10-12 18:59:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
+const cors = require('cors')
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+app.use((req, res, next) => {
+  return next({status: 404, message: 'not found'})
+})
+
+app.use((err, req, res, next) => {
+  let errCode, errMessage
+
+  if (err.errors) {
+    errCode = 400
+    const keys = Object.keys(err.errors)
+   
+    errMessage = err.errors[keys[0]].message
+  } else {
+ 
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage)
+})
